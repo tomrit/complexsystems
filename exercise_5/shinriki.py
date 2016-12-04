f92c7a77612635ce75e3e94801ced982556cedae
tom <thomas.rittmann@stud.uni-goettingen.de>
2016-12-04 02:31:02 +0100

    added b) Bifurcation Diagram for Shinriki_Oscillator. looks nice and similar to feigenbaum diagram
    we still need some self-regulating resoultion to resolve the bifurcation points up to the 32-orbit (bifurcation point number 8) as requested in assignment.

--- exercise_5/shinriki.py
+++ exercise_5/shinriki.py
@@ -52,7 +52,7 @@ def run(r0, t_max, t_step=0.001, r1=22e3):
 
 
 def plot():
-    t_max = 0.5
+    t_max = 0.25
     t_step = 1e-5
     r0 = [0, 0.5, 0.75e-3]
     dgl = run(r0, t_max, t_step)
@@ -96,26 +96,36 @@ def plot():
 
 
 def parameter_swipe():
-    t_max = 0.5
-    t_step = 1e-5
+    t_max = 0.5  # better 1.0
+    t_step = 1e-4  # 1e-5 is nicer
     r0 = [0, 0.5, 0.75e-3]
-    N = 5
-    r1s = np.linspace(19e3, 22e3, N)
+    N = 30
+    # r1s = np.linspace(20.3e3, 20.8e3, N)
+    r1s = np.linspace(19e3, 22e3,N)
     v1s = []
     rrls = []
-
+    fig_bifurc = plt.figure()
+    ax_bifurc = fig_bifurc.add_subplot(111)
     for r1 in r1s:
+        print r1
         dgl = run(r0, t_max, t_step, r1)
         rt = np.array(dgl.rt)
-        start_idx = 300
+        # change start_idx depending on resolution --> maybe better throw away fixed number of zero_crossings(prob:slower)
+        start_idx = 10000
+        print(len(rt))
         rt = rt[start_idx:]
-
         zero_crossings = get_zero_crossings(rt[:, 1])
+        # linear interpolation: very essential here! brings much more than finer resolution!
+        v2dif = rt[zero_crossings + 1, 1] - rt[zero_crossings, 1]
+        v1dif = rt[zero_crossings + 1, 0] - rt[zero_crossings, 0]
+        v2part = -rt[zero_crossings, 1]
+        v1add = v2part / v2dif * v1dif
+        v1 = rt[zero_crossings, 0] + v1add
+        # print v1
+        r_vec = np.array([copy.copy(r1)] * len(v1))
+        ax_bifurc.plot(r_vec / 1000, v1, '.r')
 
-        v1 = rt[zero_crossings, 0]
-        v1s.append(copy.copy(v1))
-        rrls.append([copy.copy(r1)] * len(v1))
-    plt.plot(r1s, v1s, '.r')
+    ax_bifurc.set_xlim(18.7,22.3)
     plt.show()
 
     def plot_colored():
@@ -145,5 +155,5 @@ def parameter_swipe():
         ax.autoscale_view()
 
 
-plot()
-# parameter_swipe()
+# plot()
+parameter_swipe()
