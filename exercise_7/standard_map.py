775f3662a45f5ff2659fec0f80cee96c6679d40b
John Henri Gaida <johnhenri.gaida@stud.uni-goettingen.de>
2016-12-14 21:43:30 +0100

    Print k values in filename

--- exercise_7/standard_map.py
+++ exercise_7/standard_map.py
@@ -153,6 +153,7 @@ if __name__ == '__main__':
         current_axis.set_ylim(p_interval)
         current_axis.set_title("k = {:.2f}".format(k))
 
-    fig1.savefig("exercise7_Standard-Map.png", dpi=300, transparent=True, bbox_inches='tight')
+    ks_string = "k = "+", ".join("{:.2f}".format(k) for k in ks)
+    fig1.savefig("exercise7_Standard-Map - {}.png".format(ks_string), dpi=300, transparent=True, bbox_inches='tight')
 
     plt.show()
